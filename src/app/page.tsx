95e81cfbb12f71ae97705678bbf7e1ea69ecc35f
AZIZXlaouiti <azizrocke@gmail.com>
2023-07-29 09:39:40 -0400

    refactor: adding utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 'use client'
 import { FC , useState} from "react";
-import Image from 'next/image'
 import styles from './page.module.css'
 import { useDraw } from "../../hooks/useDraw";
 import { ChromePicker } from 'react-color'
 import { io } from 'socket.io-client'
+import { drawLine } from "../../utils/drawLine";
 interface pageProps {}
 
-const socket = io('http://localhost:3000/')
+// establishing web socket connection 
+const socket = io("ws://localhost:3001");
 
 const page: FC<pageProps> = ({}) =>{
-  const {canvasRef, onMouseDwn ,clear} = useDraw(drawLine)
+  const {canvasRef, onMouseDwn ,clear} = useDraw(createLine)
   const [ color , setColor ] = useState<string>('#000')
   // line calculation preparing to render// 
-  function drawLine({ prevPoint ,currentPoint , ctx }:Draw){
-    
-
-  }
+  
   function createLine({ prevPoint ,currentPoint , ctx }:Draw){
-    // emit to server
+    // emit to server socket
+    socket.emit('draw-line' , ({prevPoint , currentPoint , color}))
+    drawLine({ prevPoint ,currentPoint , ctx , color})
   }
   return (
     <div className={styles.main}>
