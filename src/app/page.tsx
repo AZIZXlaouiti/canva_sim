e96315928846a91ae875055d36a5cce30b10a9f7
AZIZXlaouiti <azizrocke@gmail.com>
2023-07-28 23:49:00 -0400

    refactoring code in pages.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,42 +4,30 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { useDraw } from "../../hooks/useDraw";
 import { ChromePicker } from 'react-color'
-
+import { io } from 'socket.io-client'
 interface pageProps {}
+
+const socket = io('http://localhost:3000/')
 
 const page: FC<pageProps> = ({}) =>{
   const {canvasRef, onMouseDwn ,clear} = useDraw(drawLine)
   const [ color , setColor ] = useState<string>('#000')
   // line calculation preparing to render// 
   function drawLine({ prevPoint ,currentPoint , ctx }:Draw){
-    // connecting prevpoint with currentPoint instead of a seperate dots 
-    const { x : currX , y : currY } = currentPoint
-    const lineColor = color
-    const linWidth = 5
+    
 
-    let startPoint = prevPoint ?? currentPoint 
-    // if there is a prev point otherwise assing currentPoint
-    ctx.beginPath()
-    ctx.lineWidth = linWidth
-    ctx.strokeStyle = lineColor 
-    ctx.moveTo(startPoint.x , startPoint.y)
-    // connecting line
-    ctx.lineTo(currX , currY)
-    ctx.stroke()
-
-    ctx.fillStyle = color
-    ctx.beginPath()
-    // arc(x: number, y: number, radius: number,
-    ctx.arc(startPoint.x , startPoint.y , 2 ,0 ,2 * Math.PI) // get full circle??
-    ctx.fill()
-
+  }
+  function createLine({ prevPoint ,currentPoint , ctx }:Draw){
+    // emit to server
   }
   return (
     <div className={styles.main}>
+      <div className="container">
+      <div></div>
       <ChromePicker 
         color={color}
         onChange={(e) => setColor(e.hex)}
-      />
+        />
       <button 
         onClick={clear}>
         clear
@@ -49,7 +37,8 @@ const page: FC<pageProps> = ({}) =>{
       ref = {canvasRef}
       width={750}
       height={750}
-    />
+      />
+      </div>
     </div>
     )
 }
---
+++ utils/drawLine.ts
@@ -0,0 +1,26 @@
+type drawLineProps = Draw & {
+    // merging with other types
+    color : string
+}
+export const drawLine = ({ prevPoint ,currentPoint , ctx , color }:drawLineProps)=>{
+    // connecting prevpoint with currentPoint instead of a seperate dots 
+    const { x : currX , y : currY } = currentPoint
+    const lineColor = color
+    const linWidth = 5
+
+    let startPoint = prevPoint ?? currentPoint 
+    // if there is a prev point otherwise assing currentPoint
+    ctx.beginPath()
+    ctx.lineWidth = linWidth
+    ctx.strokeStyle = lineColor 
+    ctx.moveTo(startPoint.x , startPoint.y)
+    // connecting line
+    ctx.lineTo(currX , currY)
+    ctx.stroke()
+
+    ctx.fillStyle = color
+    ctx.beginPath()
+    // arc(x: number, y: number, radius: number,
+    ctx.arc(startPoint.x , startPoint.y , 2 ,0 ,2 * Math.PI) // get full circle??
+    ctx.fill()
+}
